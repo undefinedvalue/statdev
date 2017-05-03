30a2d69f0b7e1d5c0750fb1cd0705479f342b1ad
Anna Bell <undefinedvalue@users.noreply.github.com>
2017-05-03 13:42:02 -0700

    Fix segfault due to changing cwd

--- workflow/server.py
+++ workflow/server.py
@@ -3,14 +3,12 @@
 import SimpleHTTPServer
 import SocketServer
 import os
-from datetime import datetime
+from time import sleep
 import shutil
 import threading
 import signal
-import watchdog
-from watchdog.events import PatternMatchingEventHandler
-from watchdog.observers import Observer
 import build_pipeline.build
+from build_pipeline.util import fileutils as futil
 
 refresh_js_path = os.path.join(os.path.dirname(__file__), 'refresh.js')
 
@@ -25,43 +23,57 @@ class Server:
     self.dst_dir = dst_dir
     self.port = port
     self.final_build_dir = os.path.join(self.dst_dir, 'final')
-    self.build_sem = threading.Semaphore()
     self.httpd = None
     self.server_thread = None
+    self.watch = True
 
   def start(self):
-    # Watch the source files for changes
-    filewatch = Observer()
-    filewatch.schedule(FilewatchHandler(parent=self,
-        ignore_patterns=['*.swp', '*~']),
-        self.src_dir,
-        recursive=True)
-
     # Clean shutdown on ctrl+c
     def signal_handler(signal, frame):
       print
       print 'Shutting down...'
+      self.watch = False
       self.stop_server()
-      filewatch.stop()
 
     signal.signal(signal.SIGINT, signal_handler)
-
-    self.rebuild()
-    self.start_server()
 
     print 'Serving at port', self.port
     print 'Serving files from', self.final_build_dir
     print('Press Ctrl+C to stop')
 
-    filewatch.start()
-    signal.pause()
-    filewatch.join(5000)
+    (lastmtime, path) = self.getUpdatedFile(0)
+    self.rebuild()
+
+    while self.watch:
+      (lastmtime, path) = self.getUpdatedFile(lastmtime)
+      if path:
+        print 'File changed:', path
+        self.stop_server()
+        self.rebuild()
+        self.start_server()
+
+      sleep(0.25)
+
+  def getUpdatedFile(self, lastmtime):
+    updatedPath = None
+
+    for (root, dirs, files) in os.walk(self.src_dir):
+      for f in files:
+        if futil.ext(f) not in ['.swp'] and f != '.DS_Store':
+          path = os.path.join(root, f)
+          mtime = os.path.getmtime(path)
+
+          if mtime > lastmtime:
+            lastmtime = mtime
+            updatedPath = path
+
+    return (lastmtime, updatedPath)
 
   # Rebuilds the project, as if by "build.py build --skip s3_upload"
   def rebuild(self):
-    self.build_sem.acquire()
     try:
       print 'Building', self.src_dir
+      os.chdir(self.src_dir)
       build_pipeline.build.build(self.src_dir, self.dst_dir, skip=['s3_upload'],
           opts={'environment': 'development'})
 
@@ -73,8 +85,6 @@ class Server:
       shutil.copy(refresh_js_path, dst_js_dir)
     except Exception as e:
       print e
-    finally:
-      self.build_sem.release()
 
   def server(self):
     os.chdir(self.final_build_dir)
@@ -85,37 +95,14 @@ class Server:
       self.httpd.serve_forever()
     finally:
       self.httpd.server_close()
-      print 'Server stopped'
 
   # Runs the HTTP server in a separate thread
   def start_server(self):
-    print 'Starting server'
     self.server_thread = threading.Thread(target=lambda: self.server())
     self.server_thread.start()
 
   def stop_server(self):
     if self.httpd:
-      print 'Stopping server'
       self.httpd.shutdown()
-      self.server_thread.join(5000)
-
-
-class FilewatchHandler(PatternMatchingEventHandler):
-  def __init__(self, parent, *args, **kwargs):
-    super(FilewatchHandler, self).__init__(*args, **kwargs)
-    self.parent = parent
-    self.last_event_time = datetime.now()
-
-  # Rebuilds the project if any change is detected, unless we just rebuilt in
-  # the last second.
-  def on_any_event(self, event):
-    dt = datetime.now() - self.last_event_time
-
-    if not event.is_directory and dt.total_seconds() > 1:
-      print 'Detected change in', event.src_path
-      self.last_event_time = datetime.now()
-
-      self.parent.stop_server()
-      self.parent.rebuild()
-      self.parent.start_server()
+      self.server_thread.join()
 
